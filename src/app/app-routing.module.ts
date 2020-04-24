2185d796cb2e8440389fda9caedbb3c114db8c60
YEO <yeo_ali@yahoo.fr>
2020-04-24 11:16:57 +0000

    commit malaria

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,32 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {ScoreComponent} from './score/score.component';
+import {ConfigurationComponent} from './configuration/configuration.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ScoreComponent,
+    data: {
+      title: 'Malaria score card'
+    }
+  },
+  {
+    path: 'score',
+    component: ScoreComponent,
+    data: {
+      title: 'Malaria score card'
+    }
+  },
+  {
+    path: 'setting',
+    component: ConfigurationComponent,
+    data: {
+      title: 'Data setting'
+    }
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ConfigurationComponent } from './configuration/configuration.component';
+import { ScoreComponent } from './score/score.component';
+import {FormsModule} from '@angular/forms';
+import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfigurationComponent,
+    ScoreComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
