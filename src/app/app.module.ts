c308420776a7a4e2b249ece8475fcc7991606ed2
YEO <yeo_ali@yahoo.fr>
2020-04-27 19:13:41 +0000

    commit malaria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ConfigurationComponent } from './configuration/configuration.component';
 import { ScoreComponent } from './score/score.component';
-import {FormsModule} from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
 import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgxDhis2MenuModule} from '@hisptz/ngx-dhis2-menu';
+import {NgxDhis2HttpClientModule} from '@iapps/ngx-dhis2-http-client';
+
 
 export function HttpLoaderFactory(httpClient: HttpClient) {
   return new TranslateHttpLoader(httpClient);
@@ -21,17 +24,20 @@ export function HttpLoaderFactory(httpClient: HttpClient) {
     ScoreComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FormsModule,
-    HttpClientModule,
+    BrowserModule, AppRoutingModule, FormsModule, HttpClientModule, NgxDhis2MenuModule,
+    BrowserAnimationsModule,
+    NgxDhis2HttpClientModule.forRoot({
+      namespace: 'malariaScoreCard',
+      version: 1,
+      models: {}
+    }),
     TranslateModule.forRoot({
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
         deps: [HttpClient]
       }
-    })
+    }), ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -6,7 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./score.component.css']
 })
 export class ScoreComponent implements OnInit {
-  titleScore = 'Search Malaria score card by name';
   constructor() { }
 
   ngOnInit(): void {
