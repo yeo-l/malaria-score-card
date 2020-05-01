5607679350ef0b2d277716be5d514fcb01698e4e
YEO <yeo_ali@yahoo.fr>
2020-05-01 14:08:50 +0000

    commit malaria

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TreeviewConfig, TreeviewItem} from 'ngx-treeview';
 import {OrgUnitService} from '../services/org-unit.service';
+import {DataService} from '../services/data.service';
 
 @Component({
   selector: 'app-score',
@@ -8,9 +9,43 @@ import {OrgUnitService} from '../services/org-unit.service';
   styleUrls: ['./score.component.css']
 })
 export class ScoreComponent implements OnInit {
-  constructor() { }
+  keyword = 'name';
+ dates: any;
+ data = [
+    {
+      id: 1,
+      name: 'Dakota Gaylord PhD',
+      address: '14554 Smith Mews'
+    },
+    {
+      id: 2,
+      name: 'Maria Legros',
+      address: '002 Pagac Drives'
+    },
+    {
+      id: 3,
+      name: 'Brandyn Fritsch',
+      address: '8542 Lowe Mountain'
+    },
+    {
+      id: 4,
+      name: 'Glenna Ward V',
+      address: '1260 Oda Summit'
+    },
+    {
+      id: 5,
+      name: 'Jamie Veum',
+      address: '5017 Lowe Route'
+    }
+  ];
+  constructor( private dataSeries: DataService) { }
+
 
   ngOnInit(): void {
   }
-
+  getDataTest(){
+    this.dataSeries.getDataStore().subscribe( dat => {
+      this.dates = dat;
+    });
+  }
 }
